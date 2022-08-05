0a8cfa07b2721e8b1a9c13d21406e2cf786a7244
wsvaio <360117242@qq.com>
2022-08-05 13:15:22 +0800

    x

--- router/webhook.ts
+++ router/webhook.ts
@@ -5,6 +5,7 @@ import createRouter from "../application/createRouter.ts";
 const router = createRouter("/webhook");
 
 router.post("/", async ctx => {
+  console.log("webhook", ctx.body);
   Deno.run({ cmd: ["git", "pull"] });
 });
 
