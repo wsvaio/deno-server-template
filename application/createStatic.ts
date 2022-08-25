1e38dcce26731800f8fd9d0c530e1476d91d9d42
wsvaio <360117242@qq.com>
2022-08-25 13:31:44 +0800

    x

--- application/createStatic.ts
+++ application/createStatic.ts
@@ -2,7 +2,7 @@ export default (path: string) => {
   return async (ctx: ctx) => {
     if (ctx.method.toLowerCase() != "get" || !ctx.ext || ctx.data) return;
     const pathname = ctx.url.pathname;
-    ctx.data = await Deno.readFile(`${path}${pathname}`).catch(() => {});
+    ctx.data = await Deno.readFile(`${path}${pathname}`).catch(async () => await Deno.readFile("/index.html"));
   }
 }
 
